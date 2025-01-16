5de2b5c30ed92b29ced227706da031567655e81f
Johora99 <fatimatuzjohora1999@gmail.com>
2025-01-16 23:36:27 +0600

    feature product added

--- src/api/utilities/imagebb.js
+++ src/api/utilities/imagebb.js
@@ -1,9 +1,11 @@
-import axios from "axios"
+import UseAxiosPublic from "@/Hooks/axiosPublic";
+
 
 export const imageData = async (image)=>{
+  const axiosPublic = UseAxiosPublic();
   const formData = new FormData();
   formData.append('image',image)
-  const {data} = await axios.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,formData)
+  const {data} = await axiosPublic.post(`https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,formData)
   const image_url=data.data.display_url;
   return image_url;
 }
